29308f7e7fd0e4428a6b3d66f213bf42e5fc05fa
Wendy Pham <wendypham_s2@hotmail.com>
2022-09-22 22:13:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const title = 'Welcome to Dog Care Tips';
+  const likes = 50;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{ 10 }</p>
+      </div>
     </div>
   );
 }
