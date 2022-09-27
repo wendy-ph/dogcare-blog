83092fa8c7d692f3ab064e4943e872737d1cfc86
Wendy Pham <wendypham_s2@hotmail.com>
2022-09-26 21:24:07 -0400

    pushing to github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 const App = () => {
 
-  const title = 'Welcome to Dog Care Tips';
-  const likes = 50;
-
   return (
-    <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{ 10 }</p>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
